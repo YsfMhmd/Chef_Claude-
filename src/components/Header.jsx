5297e0d4e714e10cc20999f35f854e172025c4e9
YsfMhmd <yusufmohameda1@gmail.com>
2025-03-08 16:54:30 +0200

    Added components and styles directories and added Header.jsx containing Header component and its styling file(Header.css) in styles directory

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import "../styles/Header.css"
+export default function Header(){
+    return(
+        <header>
+            <nav>
+                <img src="../src/assets/chef-claude-logo.svg" alt="Chef Claude logo" />
+                <h1>Chef Claude</h1>
+            </nav>
+        </header>
+    )
+}
